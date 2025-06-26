b42eb88e1198c4a9284aa22085a4b8a348b453b6
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2025-06-26 11:48:31 +0530

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,12 +5,13 @@ export default function ProductCard({ product }) {
   return (
     <div className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300 border border-gray-100">
       <Image
-{product.image}
-        alt="https://www.google.com/url?sa=i&url=https%3A%2F%2Farchive.org%2Fdetails%2Fplaceholder-image&psig=AOvVaw1Oh8SEDUjlxaoDdNbDVD2X&ust=1751004033391000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPjByb20jo4DFQAAAAAdAAAAABAL"
-        width={400}
-        height={200}
-        className="w-full h-48 object-cover mb-4 rounded-md"
-      />
+  src={product.image}  
+  alt="https://www.google.com/url?sa=i&url=https%3A%2F%2Farchive.org%2Fdetails%2Fplaceholder-image&psig=AOvVaw1Oh8SEDUjlxaoDdNbDVD2X&ust=1751004033391000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPjByb20jo4DFQAAAAAdAAAAABAL"
+  width={400}
+  height={200}
+  className="w-full h-48 object-cover mb-4 rounded-md"
+/>
+
       <h2 className="text-xl font-bold text-gray-900 mb-2">{product.name}</h2>
       <p className="text-2xl font-semibold text-gray-600 mb-4">{product.price}</p>
       <Link href={`/products/${product.id}`}>
